99ac7748869dba6de2c590e943312a0b8f531935
Keenan Shumard <keenan.shumard@gmail.com>
2025-11-03 18:34:13 -0700

    Fix off by 1 bug, and java version

--- dag/src/main/java/com/usfws/dag/DAG/DagService.java
+++ dag/src/main/java/com/usfws/dag/DAG/DagService.java
@@ -7,31 +7,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class DagService {
 
-    final static int maxDestinations=3;
+    final static int maxDestinations = 3;
 
-    public static DAG generateRandomDag(int vertices){
+    public static DAG generateRandomDag(int vertices) {
         DAG dag = new DAG(vertices);
         Random r = new Random();
 
         //on each vertex
-        for (int i = 1; i < vertices; i++) {
+        for (int i = 1; i <= vertices + 1; i++) {
             //for each vertex after it, randomly assign an edge sometimes
-            for(int j = i+1;j < vertices;j++){
-                if(j == i+1 && dag.getVertices().get(j).numParents==0){
+            for (int j = i + 1; j <= vertices; j++) {
+                // if the next one currently has no parent, give it this one. 
+                if (j == i + 1 && dag.getVertices().get(j - 1).numParents == 0) {
+                    dag.addEdge(i, j);
+                } else if (r.nextBoolean()) {
                     dag.addEdge(i, j);
                 }
-                if(r.nextBoolean()){
-                    dag.addEdge(i, j);
-                }
-
             }
-
         }
-        
+
         return dag;
     }
 
-    public static DAG generateBinaryTreeDag(int vertices){
+    public static DAG generateBinaryTreeDag(int vertices) {
         DAG dag = new DAG(vertices);
         Random r = new Random();
         int level = 0;
@@ -39,15 +37,15 @@ public class DagService {
         //on each vertex
         for (int i = 1; i < vertices; i++) {
             //for each vertex after it, randomly assign an edge sometimes
-            for(int j = i+1;j < vertices;j++){
-                if(r.nextBoolean()){
+            for (int j = i + 1; j < vertices; j++) {
+                if (r.nextBoolean()) {
                     dag.addEdge(i, j);
                 }
 
             }
 
         }
-        
+
         return dag;
     }
 }
